5e65051472b27a1760e09f0676f5a1a900b995f9
trighhorn <trighhorn@gmail.com>
2021-08-08 13:05:39 -0600

    added route for portfolio

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ const cors = require("cors");
 const app = express();
 const PORT = 3000;
 
-app.use(cors());
-app.use(express.json());
-
 mongoose.connect(process.env.DATABASE_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 const db = mongoose.connection
+
+app.use(express.json());
+app.use(cors());
+
+const portfolioRouter = require('./routes/portfolio')
+app.use('/portfolio', portfolioRouter)
 
 db.on('error', (err) => console.error(err))
 db.once('open', () => console.log('Database is open and live'))
