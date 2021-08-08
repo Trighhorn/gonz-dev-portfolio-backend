cf29bcb30bc2ae3a8ec24346958aed2dc9881d84
trighhorn <trighhorn@gmail.com>
2021-08-08 14:10:51 -0600

    created a function to get portfolio item ids add the ability to get a single portfolio by id

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -16,8 +16,8 @@ router.get("/", async (req, res) => {
 
 // gets one item
 
-router.get("/:id", (req, res) => {
-  res.send(req.params.id);
+router.get("/:id", getPortfolioItem, (req, res) => {
+  res.send(res.portfolioItem)
 });
 
 // create a new item
@@ -28,7 +28,7 @@ router.post("/", async (req, res) => {
     url: req.body.url,
     description: req.body.description,
     position: req.body.position,
-    tags: req.body.tags
+    tags: req.body.tags,
   });
   try {
     const newPortfolioItem = await portfolio.save();
@@ -40,12 +40,25 @@ router.post("/", async (req, res) => {
 
 // updates an item
 
-router.patch("/:id", (req, res) => {});
+router.patch("/:id", getPortfolioItem, (req, res) => {});
 
 //deletes item
 
-router.delete("/:id", (req, res) => {});
+router.delete("/:id", getPortfolioItem, (req, res) => {});
 
+async function getPortfolioItem(req, res, next) {
+  let portfolioItem;
+  try {
+    portfolioItem = await PortfolioItem.findById(req.params.id);
+    if (portfolioItem == null) {
+      return res.status(404).json({ message: "Cannot get Portfolio Item" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 
+  res.portfolioItem = portfolioItem;
+  next();
+}
 
 module.exports = router;
