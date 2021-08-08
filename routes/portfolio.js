55af1be9b48b907f314f6e069720159ffaf5aa0a
trighhorn <trighhorn@gmail.com>
2021-08-08 13:11:11 -0600

    added boilerplate routes for CRUD functions and tested portfolio route

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,4 +1,34 @@
 const express = require("express")
 const router = express.Router()
 
+//gets all items
+
+router.get('/', (req, res) => {
+    res.send('Hello from portfolio route')
+})
+
+// gets one item
+
+router.get('/:id', (req, res) => {
+
+})
+
+// create a new item
+
+router.post('/', (req, res) => {
+
+})
+
+// updates an item
+
+router.patch('/:id', (req, res) => {
+
+})
+
+//deletes item
+
+router.delete('/:id', (req, res) => {
+
+})
+
 module.exports = router
