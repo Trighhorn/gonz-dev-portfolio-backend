41312a186fc0cb7e24b509b38717852a72a23fa0
trighhorn <trighhorn@gmail.com>
2021-08-08 14:44:16 -0600

    added the ability to update a single item

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -17,7 +17,7 @@ router.get("/", async (req, res) => {
 // gets one item
 
 router.get("/:id", getPortfolioItem, (req, res) => {
-  res.send(res.portfolioItem)
+  res.send(res.portfolioItem);
 });
 
 // create a new item
@@ -40,17 +40,43 @@ router.post("/", async (req, res) => {
 
 // updates an item
 
-router.patch("/:id", getPortfolioItem, (req, res) => {});
+router.patch("/:id", getPortfolioItem, async (req, res) => {
+  if (req.body.name != null) {
+    res.portfolioItem.name = req.body.name;
+  }
+
+  if (req.body.url != null) {
+    res.portfolioItem.url = req.body.url;
+  }
+
+  if (req.body.description != null) {
+    res.portfolioItem.description = req.body.description;
+  }
+
+  if (req.body.position != null) {
+    res.portfolioItem.position = req.body.position;
+  }
+
+  if (req.body.tags != null) {
+    res.portfolioItem.tags = req.body.tags;
+  }
+  try {
+    const updatedPortfolioItem = await res.portfolioItem.save();
+    res.json(updatedPortfolioItem);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 //deletes item
 
 router.delete("/:id", getPortfolioItem, async (req, res) => {
-    try {
-        await res.portfolioItem.remove()
-        res.json({ message: "Deleted Portfolio Item" })
-    } catch (err) {
-        return res.status(500).json({ message: err.message });
-    }
+  try {
+    await res.portfolioItem.remove();
+    res.json({ message: "Deleted Portfolio Item" });
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
 });
 
 // middle ware to get id
